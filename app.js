13c46a79e1d12541071c4baaa71c1d39254576f1
chh817 <chh817@gmail.com>
2022-05-30 11:27:52 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+// 引用 mongoose
+require("./config/mongoose")
 
 // 設定 port 監聽
 app.listen(3000, () => console.log(`App is running on http://localhost:${port}`))
---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+// 設定變數
+const mongoose = require("mongoose")
+
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on("error", () => console.log("connection error!"))
+// 連線成功
+db.once("open", () => console.log("MongoDB connected!"))
+
+module.exports = db
