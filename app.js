988f85fce2cb778afc32fc67363a15ccd038900b
chh817 <chh817@gmail.com>
2022-05-30 10:29:06 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,8 @@
+// 設定變數
+const express = require("express")
+const app = express()
+const port = 3000
+
+
+// 設定 port 監聽
+app.listen(3000, () => console.log(`App is running on http://localhost:${port}`))
